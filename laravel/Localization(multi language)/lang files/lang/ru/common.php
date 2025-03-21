260693dad9e1f55a20f22b842084e8c8267e330a
ProCode <fire2point@gmail.com>
2025-03-21 16:32:03 +0300

    0019

---
+++ laravel/Localization(multi language)/lang files/lang/ru/common.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'Main page' => 'Главная',
+    'Password' => 'Пароль',
+    'Back' => 'Назад',
+    'Save' => 'Сохранить',
+    'Saved' => 'Сохранено',
+    'Dashboard' => 'Панель инструментов',
+    'Profile' => 'Профиль',
+    'About' => 'О нас',
+    'Contacts' => 'Контакты',
+    'Profile Information' => 'Информация о профиле',
+    "Update your account's profile information and email address" => "Обновите информацию профиля вашей учетной записи и адрес электронной почты",
+];
